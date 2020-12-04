a974820edfc785d4c9b5f47a27e67069c9d009b0
cptspadge <43677995+cptspadge@users.noreply.github.com>
2020-12-04 12:23:28 -0800

    Initial Project

---
+++ MEGAJAM/Source/MEGAJAM/MEGAJAM.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "MEGAJAM.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MEGAJAM, "MEGAJAM" );
---
+++ MEGAJAM/Source/MEGAJAM/MEGAJAMGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MEGAJAMGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MEGAJAM_API AMEGAJAMGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
