9870c2d27525544a7fddbbfea9e31916aa34e556
CreatorOcelot <74989691+CreatorOcelot@users.noreply.github.com>
2020-12-05 15:55:14 -0800

    Brian
    MegaJam Source

---
+++ MEGAJAM/Source/MEGAJAM/MegaJamPlayerController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "MegaJamPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MEGAJAM_API AMegaJamPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
